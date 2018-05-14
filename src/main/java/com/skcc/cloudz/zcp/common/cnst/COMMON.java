9fdc71645a46cf4d484f7bc6f4184ea1e87a8e9d
kbtapjm <kbtapjm@gmail.com>
2018-05-14 13:50:31 +0900

    zcp-portal, zcp-iam 분리에 따른 소스 정리

--- src/main/java/com/skcc/cloudz/zcp/common/cnst/COMMON.java
+++
@@ -1,5 +0,0 @@
-package com.skcc.cloudz.zcp.common.cnst;
-
-public class COMMON {
-	public static final String SESSION_TIMEOUT_OBJ = "SESSION_TIMEOUT_OBJ";
-}
--- src/main/java/com/skcc/cloudz/zcp/common/util/KubeApi.java
+++
@@ -1,8 +0,0 @@
-package com.skcc.cloudz.zcp.common.util;
-
-import io.kubernetes.client.ApiException;
-
-public interface KubeApi {
-	Object getData() throws ApiException;
-		
-}
