f9ad21963f656221ae2c0d2da0f6dc1548d92843
Administrator <Administrator@SKCC16D00589.SKCC.NET>
2018-06-19 19:27:05 +0900

    네임스페이스 추가

--- src/main/java/com/skcc/cloudz/zcp/portal/iam/namespace/service/NamespaceService.java
+++ src/main/java/com/skcc/cloudz/zcp/portal/iam/namespace/service/NamespaceService.java
@@ -3,6 +3,7 @@ package com.skcc.cloudz.zcp.portal.iam.namespace.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -45,13 +46,31 @@ public class NamespaceService {
     }
     
     public void deleteNamespace(String namespace) throws Exception {
-        Map<String, Object> resultMap = new HashMap<String, Object>();
-        
         ApiResponseVo response = client.request(HttpMethod.DELETE, "/iam/namespace/"+ namespace, null);
         if (!response.getCode().equals(ApiResult.SUCCESS.getCode())) {
             throw new Exception(response.getMsg());
         }
     }
+
+    public Map<String, Object> getNamespaceResource(String namespace) throws Exception {
+    	if(StringUtils.isEmpty(namespace)) return null;
+    	Map<String, Object> resultMap = new HashMap<String, Object>();
+        String url = "/iam/namespace/{namespace}/resource";
+        ApiResponseVo response = client.request(HttpMethod.GET, url.replace("{namespace}", namespace), null);
+        if(!response.getCode().equals(ApiResult.SUCCESS.getCode())) {
+            throw new Exception(response.getMsg());
+        }
+        
+        resultMap.putAll(response.getData());
     
+        return resultMap;
+    }
+    
+    public void createNamespace(HashMap<String, String> data) throws Exception {
+        ApiResponseVo response = client.request(HttpMethod.PUT, "/iam/namespace", null);
+        if (!response.getCode().equals(ApiResult.SUCCESS.getCode())) {
+            throw new Exception(response.getMsg());
+        }
+    }
 
 }
