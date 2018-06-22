51eb4e43b6dcecd7618a201c895588cd06e1df78
kbtapjm <kbtapjm@gmail.com>
2018-06-22 17:17:34 +0900

    유저 관리 수정

---
+++ src/main/java/com/skcc/cloudz/zcp/portal/common/contoller/ErrorController.java
@@ -0,0 +1,44 @@
+package com.skcc.cloudz.zcp.portal.common.contoller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = ErrorController.RESOURCE_PATH)
+public class ErrorController {
+    
+    static final String RESOURCE_PATH = "/error";
+    
+    /**
+     * 404 not found
+     * @return
+     * @throws Exception
+     */
+    @GetMapping(value = "/notFound", consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    public String notFound() throws Exception {
+        return "common/error/notFound";
+    }
+    
+    /**
+     * 500 internal server
+     * @return
+     * @throws Exception
+     */
+    @GetMapping(value = "/serverError", consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    public String serverError() throws Exception {
+        return "common/error/serverError";
+    }
+    
+    /**
+     * 401 Unauthorized
+     * @return
+     * @throws Exception
+     */
+    @GetMapping(value = "/accessDenied", consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    public String accessDenied() throws Exception {
+        return "common/error/accessDenied";
+    }
+
+}
