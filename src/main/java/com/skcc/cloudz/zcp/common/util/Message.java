cf6940a794af7a9f2846459cb3eef63ff93908cd
cocopop1106 <zhzh1300>
2018-06-27 18:44:41 +0900

    rule v1

---
+++ src/main/java/com/skcc/cloudz/zcp/common/util/Message.java
@@ -0,0 +1,28 @@
+package com.skcc.cloudz.zcp.common.util;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+	@Autowired
+	private MessageSource messageSource;
+	private MessageSourceAccessor accessor;
+
+	@PostConstruct
+	private void init() {
+		accessor = new MessageSourceAccessor(messageSource, Locale.getDefault());
+	}
+
+	public String get(String code) {
+		return accessor.getMessage(code);
+	}
+
+}
