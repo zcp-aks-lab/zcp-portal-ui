3e0b271859402e7425cced106742f5582ff74234
kbtapjm <kbtapjm@gmail.com>
2018-07-23 17:28:19 +0900

    bug fixed

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -117,4 +117,29 @@ function setCookie(name, value, expiredays) {
     $.cookie(name, value, { expires: expiredays, path: '/' });
 }
 
+function chkPwd(str) {
+	var pw = str;
+	var num = pw.search(/[0-9]/g);
+	var eng = pw.search(/[a-z]/ig);
+	var spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+	if (pw.length < 8 || pw.length > 20) {
+		//alert("8자리 ~ 20자리 이내로 입력해주세요.");
+		return false;
+	}
+
+	if(pw.search(/₩s/) != -1){
+	  //alert("비밀번호는 공백업이 입력해주세요.");
+	  return false;
+	} 
+	
+	if(num < 0 || eng < 0 || spe < 0 ){
+	  //alert("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
+	  return false;
+	}
+	 
+	return true;
+}
+
+
 
