c8a07befc43254ee885d44585bc586aef5ca0b1f
yanghoon <ssang276@hanmail.net>
2018-11-13 18:29:43 +0900

    iam까지 relay

--- src/main/java/com/skcc/cloudz/zcp/portal/console/controller/DummyConsoleController.java
+++ src/main/java/com/skcc/cloudz/zcp/portal/console/controller/DummyConsoleController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+/*
+ * https://stackoverflow.com/a/38458914
+ */
 @Controller
-@RequestMapping(value = DummyConsoleController.RESOURCE_PATH)
+@RequestMapping(value = DummyConsoleController.RESOURCE_PATH, headers = "Connection!=Upgrade")
 public class DummyConsoleController {
     static final String RESOURCE_PATH = "/api";
     static final Pattern pattern = Pattern.compile("/");
     
-    @GetMapping("**")
-    public String initialize(HttpServletRequest req) throws Exception {
+    @GetMapping(value = {"/cluster/**", "/pod/**"})
+    public String dummy(HttpServletRequest req) throws Exception {
         ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
         String path = builder.build().getPath();
         String redirect = path.substring("/api/".length()).replace('/', '.') + ".json";
