cbdbe5a0ddd37dd76185d1d93568f4d74c11d8ad
yanghoon <ssang276@hanmail.net>
2018-12-18 14:18:25 +0900

    env label 설정 추가

--- k8s/template/setenv.sh
+++ k8s/template/setenv.sh
@@ -8,5 +8,8 @@ namespace=console
 image=cloudzcp/zcp-portal-ui:1.1.0-alpha
 replicas=0
 
+config_product=       #eg. 'ZCP :)'
+config_label=pou-dev  #eg. 'pou-dev'
+
 private_alb=$(kubectl get deploy -n kube-system | grep private | cut -f1 -d' ' | head -n 1)
 private_alb_enable='#'  # $([ -z "$private_alb" ] && echo '#' || echo '' )
