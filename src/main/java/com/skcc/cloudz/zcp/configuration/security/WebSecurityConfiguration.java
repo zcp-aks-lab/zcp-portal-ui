f3018e5a20ac3ebb6cd3cdc7125be9b6974a779a
yanghoon <ssang276@hanmail.net>
2019-01-30 14:19:38 +0900

    Integration with new version of console (node)

--- src/main/java/com/skcc/cloudz/zcp/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/skcc/cloudz/zcp/configuration/security/WebSecurityConfiguration.java
@@ -1,20 +1,36 @@
 package com.skcc.cloudz.zcp.configuration.security;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.DelegatingAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.StringUtils;
 
 import com.skcc.cloudz.zcp.common.security.filter.OpenIdConnectFilter;
@@ -23,53 +39,63 @@ import com.skcc.cloudz.zcp.common.security.handler.LogoutHandler;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
-    @Resource(name="keycloakOpenIdTemplate")
+
+    @Resource(name = "keycloakOpenIdTemplate")
     private OAuth2RestTemplate restTemplate;
-    
+
     @Autowired
     private LogoutHandler logoutHandler;
 
     @Autowired
     private SecurityProperties security;
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
-        http
-            .addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
-            .addFilterAfter(openIdConnectFilter(), OAuth2ClientContextFilter.class);
-    
-        http
-            .httpBasic()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-            .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessHandler(logoutHandler)
-            .and()
-                .csrf().disable();
-    
-        http
-            .authorizeRequests()
-                .antMatchers("/error/accessDenied").permitAll()
-                .anyRequest()
-                .authenticated()
-            .and()
-                .headers()
-                .addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy","script-src 'self'"));
-    
-        http
-            .exceptionHandling()
-            .accessDeniedPage("/error/accessDenied");
+
+        http.addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+                .addFilterAfter(openIdConnectFilter(), OAuth2ClientContextFilter.class);
+
+        http.httpBasic().authenticationEntryPoint(entryPoint()).and().logout().logoutUrl("/logout")
+                .logoutSuccessHandler(logoutHandler).and().csrf().disable();
+
+        http.authorizeRequests().antMatchers("/error/accessDenied").permitAll().anyRequest().authenticated().and()
+                .headers().addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy", "script-src 'self'"));
+
+        http.exceptionHandling().accessDeniedPage("/error/accessDenied");
     }
-    
+
     @Bean
     public OpenIdConnectFilter openIdConnectFilter() {
         final OpenIdConnectFilter filter = new OpenIdConnectFilter("/login");
         filter.setRestTemplate(restTemplate);
-        
+
         return filter;
+    }
+
+    @Bean
+    public AuthenticationEntryPoint entryPoint() {
+        /**
+         * @see HttpBasicConfigurer
+         */
+        RequestMatcher X_REQUESTED_WITH = new RequestHeaderRequestMatcher("X-Requested-With", "XMLHttpRequest");
+        RequestMatcher ACCEPT_APPLICATION_JSON = new RequestMatcher() {
+            public boolean matches(HttpServletRequest request) {
+                List<MediaType> types = MediaType.parseMediaTypes(request.getHeader("Accept"));
+                return types.contains(MediaType.APPLICATION_JSON);
+            }
+        };
+        AuthenticationEntryPoint unauthorized = new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
+
+        // for ajax
+        LinkedHashMap<RequestMatcher, AuthenticationEntryPoint> entryPoints = new LinkedHashMap<RequestMatcher, AuthenticationEntryPoint>();
+		entryPoints.put(X_REQUESTED_WITH, unauthorized);
+		entryPoints.put(ACCEPT_APPLICATION_JSON, unauthorized);
+
+        // for form
+        DelegatingAuthenticationEntryPoint entryPoint = new DelegatingAuthenticationEntryPoint(entryPoints);
+        entryPoint.setDefaultEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+
+        return entryPoint;
     }
     
     @Override
--- src/main/java/com/skcc/cloudz/zcp/portal/console/controller/DummyConsoleController.java
+++ src/main/java/com/skcc/cloudz/zcp/portal/console/controller/DummyConsoleController.java
@@ -1,18 +1,35 @@
 package com.skcc.cloudz.zcp.portal.console.controller;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.skcc.cloudz.zcp.api.iam.domain.vo.ZcpUserVo;
+import com.skcc.cloudz.zcp.common.constants.ClusterRole;
+import com.skcc.cloudz.zcp.common.domain.vo.AddOnServiceMataVo;
+import com.skcc.cloudz.zcp.common.domain.vo.AddOnServiceMataVo.Role;
+import com.skcc.cloudz.zcp.common.security.vo.OpenIdConnectUserDetailsVo;
+import com.skcc.cloudz.zcp.configuration.web.interceptor.AddOnServiceMetaDataInterceptor;
+import com.skcc.cloudz.zcp.portal.management.user.service.UserService;
+import com.skcc.cloudz.zcp.portal.my.service.MyService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -23,11 +40,20 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping(value = DummyConsoleController.RESOURCE_PATH, headers = "Connection!=Upgrade")
 public class DummyConsoleController {
     static final String RESOURCE_PATH = "/api";
-    static final Pattern pattern = Pattern.compile("/");
-    
+
     private String redirect = "/iam/rbac/{username}/namespace/{ns}/{kind}";
-    
-    @GetMapping(value = {"/cluster/**", "cluster/list"})
+    private Multimap<String, AddOnServiceMataVo> meta = ArrayListMultimap.create();
+
+    @Autowired
+    private MyService myService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AddOnServiceMetaDataInterceptor metaService;
+
+    @GetMapping(value = { "/cluster/**", "cluster/list" })
     public String dummy(HttpServletRequest req) throws Exception {
         ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
         String path = builder.build().getPath();
@@ -39,29 +65,87 @@ public class DummyConsoleController {
         return "redirect:/dummy/" + redirect;
     }
 
-    @GetMapping(value = {"/cluster/{cs}/namespace/list"})
+    @GetMapping(value = { "/cluster/{cs}/namespace/list" })
     public String namespaceList(Principal principal, @RequestParam Map<String, String> params) throws Exception {
         params.put("username", principal.getName());
 
-    	String url = UriComponentsBuilder.fromPath("/iam/rbac/{username}/namespace")
-            .buildAndExpand(params)
-            .toString();
+        String url = UriComponentsBuilder.fromPath("/iam/rbac/{username}/namespace").buildAndExpand(params).toString();
 
         return "redirect:" + url;
     }
 
-    @GetMapping(value = {"/{kind}/list"})
-    public String redirect(Principal principal,
-            @PathVariable String kind,
-            @RequestParam Map<String, String> params) throws Exception {
+    @GetMapping(value = { "/{kind}/list" })
+    public String redirect(Principal principal, @PathVariable String kind, @RequestParam Map<String, String> params)
+            throws Exception {
         params.put("username", principal.getName());
         params.put("kind", kind);
 
-    	String url = UriComponentsBuilder.fromPath(redirect)
-            .buildAndExpand(params)
-            .toString();
-
+        String url = UriComponentsBuilder.fromPath(redirect).buildAndExpand(params).toString();
 
         return "redirect:" + url;
+    }
+
+    @GetMapping(value = { "/resource/{kind}/{name}" })
+    public String redirectResource(Principal principal, @PathVariable String kind, @PathVariable String name,
+            @RequestParam MultiValueMap<String, String> params)
+            throws Exception {
+        Map<String, String> vars = Maps.newHashMap();
+        vars.put("username", principal.getName());
+        vars.put("kind", kind);
+        vars.put("name", name);
+        vars.put("ns", params.getFirst("ns"));
+
+        String url = UriComponentsBuilder.fromPath(redirect + "/" + name)
+                            .queryParams(params)
+                            .buildAndExpand(vars)
+                            .toString();
+
+        return "redirect:" + url;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/profile")
+    public ZcpUserVo profile() throws Exception {
+        return myService.getMyUser();
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/menu")
+    public Collection<AddOnServiceMataVo> menu(@RequestParam String namespace) throws Exception {
+        Collection<AddOnServiceMataVo> menu = null;
+        OpenIdConnectUserDetailsVo user = (OpenIdConnectUserDetailsVo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String role = user.getClusterRole();
+        if(isSupserUser(role)){
+            menu = meta.get(role);
+        } else {
+            String id = user.getUserId();
+            role = userService.getNamespaceRole(namespace, id);
+            menu = meta.get(role);
+        }
+
+        // validation
+        return menu == null ? meta.get(ClusterRole.MEMBER.toString()) : menu;
+    }
+
+    @PostConstruct
+    @PutMapping(value = "/menu")
+    public void reloadMenus(){
+        meta.clear();
+        List<AddOnServiceMataVo> raw = metaService.getAddOnServiceMetaDataFileLoad();
+        for(AddOnServiceMataVo m : raw) {
+            Role roles = m.getRole();
+            for (String cluster : roles.getClusterRoles()) {
+                meta.put(cluster, m);
+            }
+
+            for (String namespace : roles.getNamespaceRoles()) {
+                meta.put(namespace, m);
+            }
+        }
+    }
+
+    private boolean isSupserUser(String csRole) {
+        return ClusterRole.CLUSTER_ADMIN.getName().equals(csRole);
     }
 }
