0be9c9747479a2d5e18cf8cd3eaa4e5055929dae
Hoon Yang <hoon@Hoonui-MacBook-Pro.local>
2019-02-28 10:34:00 +0900

    Fix Typing Error

--- k8s/template/setenv.sh
+++ k8s/template/setenv.sh
@@ -5,11 +5,11 @@ domain_iam=${domain_prefix}iam.cloudzcp.io
 domain_console=${domain_prefix}console.cloudzcp.io
 
 namespace=zcp-system
-image=registry.au-syd.bluemix.net/cloudzcp/zcp-iam:1.1.0
+image=registry.au-syd.bluemix.net/cloudzcp/zcp-portal-ui:1.1.0
 replicas=1
 
 config_product=ZCP    #eg. 'ZCP :)'
-config_label=pou-dev  #eg. 'pou-dev'
+config_label=pog-dev  #eg. 'pou-dev'
 
 private_alb=$(kubectl get deploy -n kube-system | grep private | cut -f1 -d' ' | head -n 1)
 private_alb_enable='#'  # $([ -z "$private_alb" ] && echo '#' || echo '' )
